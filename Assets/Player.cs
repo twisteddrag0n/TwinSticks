7510f3f484864df45e9ad5a282ed777f9abf5387
Lee Hall <twist3ddrag0n@gmail.com>
2015-12-06 04:57:40 -0500

    Crossplatforminput

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	float moveBall;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveBall=CrossPlatformInputManager.GetAxis ("Horizontal");
+		float amount = transform.position.z + moveBall;
+		transform.position = new Vector3 (transform.position.x, transform.position.y, amount);
+	}
+}
